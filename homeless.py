9c1f7643171fbd15cc7e1e886d9103927321f992
luimramirezm <luiramirezm@gmail.com>
2022-02-09 17:23:49 -0500

    modificado homeless y random_road

--- homeless.py
+++ homeless.py
@@ -1,15 +1,68 @@
 import random
+"""actividad en clase del bagabundo"""
+class Homeless:
+    def __init__(self, name,x=0,y=0):
+        self.name = name
+        self.x = x
+        self.y = y
 
-class Homeless():
-    
-    def __init__(self, name):
-        self.name = name 
-        
+
+    def posicion (self):
+        return (self.x,self.y)
+
+    def distance(self,other_coordinate):
+        return (self.x**2 + self.y**2)**0.5
+
 class StandarHomeless(Homeless):
-    
-    def __init__(self, name):
-        super().__init__ (name)
-    
+    def __innit__(self, name):
+        super().__init__(name)
+
     def walk(self):
-        return random.choice([(1,0), (-1,0), (0,1), (0,-1)])
+        dx,dy= random.choice([(1,0),(-1,0),(0,1),(0,-1)])
+        self.x +=dx
+        self.y +=dy
+        return [self.x,self.y]
+
+
+class ModerateHomeless(Homeless):
+    def __init__(self, name):
+        super().__init__(name)
+    def walk(self):
+        dx,dy = random.choice([(0,2),(0,-2),(2,0),(-2,0)])
+        self.x +=dx
+        self.y +=dy
+        return [self.x,self.y]
+
+
+
+class leftHomeless(Homeless):
+    def __init__(self, name):
+        super().__init__(name)
+    def walk(self):
+        dx,dy = random.choice([(0,1),(0,-1),(1,0),(-5,0)])
+        self.x +=dx
+        self.y +=dy
+        return [self.x,self.y]
+    
+class RightHomeless(Homeless):
+        def __init__(self, name):
+            super().__init__(name)
+        
+        def walk(self):
+            dx,dy = random.choice([(0,5),(0,-1),(5,0),(-1,0)])
+            self.x +=dx
+            self.y +=dy
+            return [self.x,self.y]
+
+    
+
+"""Super es para heredar de la clase padre"""
+
+
+
+"""el random.choice tiene 2 argumentos 1,0. pero puede tener mas de estos como unos 5 o 6 dimensiones o datos"""
+
+"""git branch nombre = crear nueva master.
+git branch = visualizar las master.
+git checkout master = cambia de master"""
     
