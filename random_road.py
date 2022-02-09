1883cd53a4c4dd6cec2aa5f9fd07863855b85fd2
luimramirezm <luiramirezm@gmail.com>
2022-02-09 18:55:14 -0500

    creamos las funciones para pintar los homeless con al libreria bokeh

--- random_road.py
+++ random_road.py
@@ -1,32 +1,56 @@
-from homeless import Homeless, StandarHomeless
+from imp import source_from_cache
+from turtle import distance
+from homeless import Homeless, StandardHomeless, ModerateHomeless, leftHomeless
 from coordinate import Coordinate
 from field import Field
 
 from bokeh.plotting import  figure , output_file, show
 
-def walking(field, homeless, steps):
-    begin = field.get_coordinate(homeless)
+def know_type_homeless(type_homeless):
+    if type_homeless.__name__ == StandardHomeless:
+        return "Vagabundo es Estandar"
+    elif type_homeless.__name__==ModerateHomeless:
+        return "Vagabundo Moderado"
+    else:
+        return "Vagabundo Izquierdista"
+    
 
-    for _ in range(steps):
-        field.move_homeless(homeless)
+def walking( homeless, steps, type_homeless):
+    begin = [homeless.posicion()]
+    x_graph = [0]
+    y_graph = [0]
 
-    return begin.distance(field.get_coordinate(homeless))
+    for _ in range(steps-1):
+        homeless.walk()
+        x, y =homeless.posicion()
+        x_graph.append(x)
+        y_graph.appemd(y)
+    know_homeless = (type_homeless)
+    graph(x_graph, y_graph, know_homeless, steps)
+
+    return homeless.distance_origin()
 
 def simulate_walk(steps, number_of_attemps, type_homeless):
-    homeless = type_homeless(name="Ivan")
-    begin = Coordinate(0,0)
+    homeless = []
     distance = []
-    for _ in range(number_of_attemps):
-        field = Field()
-        field.add_homeless(homeless, begin)
-        simulation_walk = walking(field, homeless, steps)
-        distance.append(round(simulation_walk, 1))
+    
+    for i in range(number_of_attemps):
+        homeless.append(type_homeless(name=f"Rastacuando {i}"))
+        emulate_walk= walking(homeless[i], steps, type_homeless)
+        distance.append(round(emulate_walk, 1))
 
     return distance
 
-def graph(x, y):
-    paint = figure(title="Camino aleatorio", x_axis_label="Pasos", y_axis_label="")
-    paint.line(x , y, legend = "Distancia")
+def graph(x_graph, y_graph, know, steps):
+    paint = figure(title="know", x_axis_label="Pasos", y_axis_label="Distancia")
+    paint.line(x_graph , y_graph, legend_label =str(steps)+"Pasos")
+    final_x= x_graph[-1]
+    final_y= y_graph[-1]
+    paint.diamond_cross(0, 0, fill_color="green", line_color="green", size=18)
+    paint.diamond_cross(final_x, final_y, fill_color="red", line_color="red", size=18)
+    final_stretch_x = [0, final_x]
+    final_stretch_y = [0, final_y]
+    graph.line(final_stretch_x, , line_width=2, color="red")
     show(paint)
 
 def main(walk_distance, number_of_attemps, type_homeless):
