2025d287fae85342431c34ff2d701c26fcb4ed2a
luiramirezm <luimramirez11@misena.edu.co>
2022-02-09 15:00:57 -0500

    final desatrazada

--- field.py
+++ field.py
@@ -1,17 +1,20 @@
-class Field():
-    
+class Field:
+
     def __init__(self):
         self.homeless_coordinates = {}
-            
+
     def add_homeless(self, homeless, coordinate):
-        self.homeless_cordinates[homeless] = coordinate 
-        
+        self.homeless_coordinates[homeless] = coordinate
+
     def move_homeless(self, homeless):
         delta_x, delta_y = homeless.walk()
-        
+
+        #path_x.append(path_x[-1] + delta_x)
+        #path_y.append(path_y[-1] + delta_y)
+
         current_coordinate = self.homeless_coordinates[homeless]
         new_coordinate = current_coordinate.move(delta_x, delta_y)
-        self.homeless_coordinates[homeless] = new_coordinate 
-        
+        self.homeless_coordinates[homeless] = new_coordinate
+
     def get_coordinate(self, homeless):
-        return self.homeless_coordinates
+        return self.homeless_coordinates[homeless]
--- random_road.py
+++ random_road.py
@@ -1,49 +1,50 @@
-from numpy import source
-from homeless import StandarHomeless 
-from field import Field
+from homeless import Homeless, StandarHomeless
 from coordinate import Coordinate
-from bokeh.plotting import figure,output_file,show
+from field import Field
 
+from bokeh.plotting import  figure , output_file, show
 
-def walking(field,homeless,steps):
+def walking(field, homeless, steps):
     begin = field.get_coordinate(homeless)
-    for _ in range (steps):
+
+    for _ in range(steps):
         field.move_homeless(homeless)
 
     return begin.distance(field.get_coordinate(homeless))
 
-def simulate_walk(steps,number_of_attemps,type_of_attemps):
-    homeless = type_of_attemps(name='Ivan')
+def simulate_walk(steps, number_of_attemps, type_homeless):
+    homeless = type_homeless(name="Ivan")
     begin = Coordinate(0,0)
     distance = []
-
-    for _ in range (number_of_attemps):
+    for _ in range(number_of_attemps):
         field = Field()
-        field.add_homeless(homeless,begin)
-        simulation_walk  = walking(field,homeless,steps)
-        distance.append(round(simulation_walk,1))
+        field.add_homeless(homeless, begin)
+        simulation_walk = walking(field, homeless, steps)
+        distance.append(round(simulation_walk, 1))
+
     return distance
 
-def graph(x,y):
-    paint = figure(title = "Camino Aleatorio", x_axis_label = "Pasos",y_axis_label  ="Distancia")
-    paint.line(x,y,legend_label = "Distancia")
+def graph(x, y):
+    paint = figure(title="Camino aleatorio", x_axis_label="Pasos", y_axis_label="")
+    paint.line(x , y, legend = "Distancia")
     show(paint)
 
-def main(walk_distance,number_of_attemps,type_homeless):
+def main(walk_distance, number_of_attemps, type_homeless):
     average_walking_distance = []
     for steps in walk_distance:
-        distance = simulate_walk(steps,number_of_attemps,type_homeless)
-        distance_average =round(sum(distance)/len(distance))
+        distance = simulate_walk(steps, number_of_attemps, type_homeless)
+        distance_average = round(sum(distance)/len(distance))
         distance_max = max(distance)
-        distance_min = min(distance)
+        distance_min = max(distance)
         average_walking_distance.append(distance_average)
-        print(f"{type_homeless.__name__}caminata aleatoria")
+        print(f"{type_homeless.__name__} caminata aleatoria")
         print(f"Media = {distance_average}")
         print(f"Max = {distance_max}")
         print(f"Min = {distance_min}")
-        graph(walk_distance,average_walking_distance)
 
-if __name__ == "__main__":
-    walk_distance = [10,100,1000,10000]
+        graph(walk_distance, average_walking_distance)
+
+if __name__ == '__main__':
+    walk_distance = [10, 100, 1000, 10000]
     number_of_attemps = 100
-    main(walk_distance,number_of_attemps,StandarHomeless)
+    main(walk_distance, number_of_attemps, StandarHomeless)
